f5f6a48eca6f75f8dd14662fc9f494a4c563e969
FilipeamTeixeira <Filipe.MarquesTeixeira@Ugent.be>
2022-04-24 11:26:14 +0200

    run build

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -1,8 +1,8 @@
 export const manifest = {
 	appDir: "internal",
-	assets: new Set([".DS_Store",".nojekyll","favicon.png","pictures/banner.jpg","pictures/elon.jpeg","pictures/logo_economics.png","pictures/logo_political.png","pictures/logo_sciences.png","pictures/logo_ugent.png","pictures/neil.jpeg","pictures/train1.jpg","pictures/train1_old.jpg","pictures/train2.jpg","pictures/train3.jpg","pictures/train3_old.jpg"]),
+	assets: new Set([".DS_Store",".nojekyll","favicon.png","pictures/background.png","pictures/banner.jpg","pictures/elon.jpeg","pictures/logo_economics.png","pictures/logo_political.png","pictures/logo_sciences.png","pictures/logo_ugent.png","pictures/neil.jpeg","pictures/svelte-logo.svg","pictures/train1.jpg","pictures/train1_old.jpg","pictures/train2.jpg","pictures/train3.jpg","pictures/train3_old.jpg"]),
 	_: {
-		mime: {".png":"image/png",".jpg":"image/jpeg",".jpeg":"image/jpeg"},
+		mime: {".png":"image/png",".jpg":"image/jpeg",".jpeg":"image/jpeg",".svg":"image/svg+xml"},
 		entry: {"file":"start-7396e05b.js","js":["start-7396e05b.js","chunks/index-891b74ed.js"],"css":["assets/start-61d1577b.css"]},
 		nodes: [
 			() => import('./nodes/0.js'),
